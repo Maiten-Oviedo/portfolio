24c7d1ae3368bd67ed87bc0a37d26f5e4171846d
Maiten-Oviedo <maitenoviedo513@gmail.com>
2026-03-25 18:24:06 -0300

    feat: create a new base layout component with comprehensive SEO metadata and configure the Astro site URL.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://maitendev.vercel.app',
   vite: {
     plugins: [tailwindcss()]
   }
